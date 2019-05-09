7dd3ee2150d1a6250ab6082f480564f250fcca52
William Chan <wchan1992@gmail.com>
2019-05-08 22:41:19 -0700

    Complete React components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,204 @@
 
 
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      page: 0
+    }
+    this.changePage = this.changePage.bind(this);
+  }
+
+  changePage(pageNum) {
+    this.setState({
+      page: pageNum
+    })
+  }
+
+  render() {
+    let currentPage = this.state.page;
+    let showPage;
+    
+    if (currentPage === 0) {
+      showPage = <div><button onClick={ () => {this.changePage(1)} }>Checkout</button></div>
+    } else if (currentPage === 1) {
+      showPage = <FormOne changePage={this.changePage} />
+    } else if (currentPage === 2) {
+      showPage = <FormTwo changePage={this.changePage} />
+    } else if (currentPage === 3) {
+      showPage = <FormThree changePage={this.changePage} />
+    } else if (currentPage === 4) {
+      showPage = <Confirmation changePage={this.changePage} />
+    }
+
+    return (
+      showPage
+    )
+  }
+}
+
+
+class FormOne extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+
+  this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  // handleSubmit () {
+  //   this.props.changePage(2);
+  // }
+
+  render() {
+    return (
+      <form onSubmit={ () => {this.props.changePage(2)} }>
+        <div>
+          <label> Name
+            <input name="name" value={this.state.name} onChange={this.handleChange} />
+          </label>
+        </div>  
+        <div>
+          <label> Email
+            <input name="email" value={this.state.email} onChange={this.handleChange} />
+          </label>
+        </div>
+        <div>
+          <label> Password
+            <input name="password" value={this.state.password} onChange={this.handleChange} />
+          </label>
+        </div>  
+        <button>Next</button>
+      </form>
+    )
+  }
+}
+
+
+class FormTwo extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      address1: '',
+      address2: '',
+      city: '',
+      state: '',
+      zipcode: '',
+      phone: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  // handleSubmit (e) {
+  //   this.props.changePage(2);
+  // }
+
+  render() {
+    return (
+      <form onSubmit={ () => {this.props.changePage(3)} }>
+        <label> Address Line 1
+          <input name="address1" value={this.state.address1} onChange={this.handleChange} />
+        </label>
+        <label> Address Line 2
+          <input name="address2" value={this.state.address2} onChange={this.handleChange} />
+        </label>
+        <label> City
+          <input name="city" value={this.state.city} onChange={this.handleChange} />
+        </label>
+        <label> State
+          <input name="state" value={this.state.state} onChange={this.handleChange} />
+        </label>
+        <label> Zip Code
+          <input name="zipcode" value={this.state.zipcode} onChange={this.handleChange} />
+        </label>
+        <label> Phone
+          <input name="phone" value={this.state.phone} onChange={this.handleChange} />
+        </label>
+        <button>Next</button>
+      </form>
+    )
+  }
+}
+
+
+class FormThree extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      creditcardnumber: '',
+      cvv: '',
+      zipcode: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  // handleSubmit () {
+  //   e.preventDefault();
+  // }
+
+  render() {
+    return (
+      <form onSubmit={ () => {this.props.changePage(4)} }>
+        <label> Credit Card Number
+          <input name="creditcardnumber" value={this.state.creditcardnumber} onChange={this.handleChange} />
+        </label>
+        <label> Expiry Date
+          <input name="expirydate" value={this.state.expirydate} onChange={this.handleChange} />
+        </label>
+        <label> CVV
+          <input name="cvv" value={this.state.cvv} onChange={this.handleChange} />
+        </label>
+        <button>Next</button>
+      </form>
+    )
+  }
+}
+
+
+class Confirmation extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div> 
+        <h1>Confirmation Page</h1>
+        <button onClick={ () => {this.props.changePage(0)} }>Purchase</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+// const db = require('./db');
 
 
+app.use(express.static('public'));
+
+// GET Route
+// app.get(
+//   //TODO
+// )
+
+// // POST Route
+// app.post(
+//   //TODO
+// )
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
